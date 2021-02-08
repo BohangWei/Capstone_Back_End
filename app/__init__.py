9364e5eb42918f12ec48d5f4c68d2cee42842ccc
Faris Rehman <faris.a.rehman@gmail.com>
2021-02-07 22:07:07 -0500

    set up login and authentication endpoints

--- app/__init__.py
+++ app/__init__.py
@@ -4,15 +4,17 @@ import app.api as api
 import app.auth as auth
 from . import db
 import os
+from flask_jwt_extended import JWTManager
 
 def create_app(test_config = None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
+    jwt_manager = JWTManager(app)
 
     #Add configuration values
     app.config.from_mapping(
         SECRET_KEY = 'dev', #Secret key, should be replaced with something random for more security
-        DATABASE = os.path.join(app.instance_path, 'flaskr.sqlite') #Database path
+        DATABASE = os.path.join(app.instance_path, 'binder.sqlite') #Database path
     )
 
     if test_config is None:
--- app/auth/users.py
+++ app/auth/users.py
@@ -5,6 +5,9 @@ from flask import (
     jsonify
 )
 from app.db import get_db
+from flask_jwt_extended import (
+    jwt_required, create_access_token, get_jwt_identity
+)
 
 bp = Blueprint('users', __name__, url_prefix='/user')
 
@@ -25,7 +28,31 @@ Returns:
 """
 @bp.route('/register', methods = ['POST'])
 def register():
-    pass
+    params = request.get_json()
+    user = params['username']
+    pwd = params['password']
+
+    #Validate the input
+    if not user:
+        return {
+            'error': 'no username given'
+        }
+    elif not pwd:
+        return {
+            'error': 'no password given'
+        }
+
+    #Store user into DB
+    query_str = 'INSERT INTO user (username, password) VALUES (?, ?)'
+    db = get_db()
+    db.execute(query_str, (user, generate_password_hash(pwd)))
+    db.commit()
+
+    #Return the JWT
+    access_token = create_access_token(identity = user)
+    return {
+        'access_token': access_token
+    }
 
 """
 This is the POST request for logging a user in and obtaining the JWT.
@@ -44,4 +71,32 @@ Returns:
 """
 @bp.route('/login', methods = ['POST'])
 def login():
-    pass
+    params = request.get_json()
+    user = params['username']
+    pwd = params['password']
+    db = get_db()
+    check_user_query = 'SELECT * FROM user WHERE username = ?'
+
+    #Validate the input
+    if not user:
+        return {
+            'error': 'no username given'
+        }
+    elif not pwd:
+        return {
+            'error': 'no password given'
+        }
+
+    #Make sure the provided credentials match up to a user
+    user_matches = db.execute(check_user_query, (user, ))
+    user_query_result = user_matches.fetchone()
+    if not check_password_hash(user_query_result['password'], pwd):
+        return {
+            'error': 'incorrect password'
+        }
+
+    #Return the JWT
+    access_token = create_access_token(identity = user)
+    return {
+        'access_token': access_token
+    }
